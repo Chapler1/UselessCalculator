51cab31d04457cc55da94d1f06da655dc2e964b9
Chapler1 <79654273+Chapler1@users.noreply.github.com>
2021-03-04 16:31:10 -0500

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Useless Calculator: A calculator that does not function. Enjoy!")
+name = input("Enter your name: ")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+print("Hi",name, "the sum of", num1, "and", num2, "is",(num1*num1*num2-num2-7))
